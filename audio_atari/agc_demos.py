c105d53e2ce8769e8753e31f9031c4bc0f87f0fe
sahiljain11 <sahilbot1@gmail.com>
2021-10-15 23:12:39 -0700

    Added annotations and heatmaps

--- audio_atari/agc_demos.py
+++ audio_atari/agc_demos.py
@@ -19,7 +19,7 @@ def GrayScaleWarpImage(image):
     #frame = np.expand_dims(frame, -1)
     return frame
 
-def MaxSkipAndWarpFrames(trajectory_dir):
+def MaxSkipAndWarpFrames(trajectory_dir, annotations, heatmaps):
     """take a trajectory file of frames and max over every 3rd and 4th observation"""
     num_frames = len(listdir(trajectory_dir))
     skip=4
@@ -29,6 +29,8 @@ def MaxSkipAndWarpFrames(trajectory_dir):
     pic = cv2.imread(image_path)
     obs_buffer = np.zeros((2,)+pic.shape, dtype=np.uint8)
     max_frames = []
+    anns = []
+    maps = []
     for i in range(num_frames):
         if i % skip == skip - 2:
             obs = cv2.imread(path.join(trajectory_dir, str(i) + ".png"))
@@ -37,25 +39,32 @@ def MaxSkipAndWarpFrames(trajectory_dir):
             obs = cv2.imread(path.join(trajectory_dir, str(i) + ".png"))
             obs_buffer[1] = obs
             #warp max to 80x80 grayscale
-            image = obs_buffer.max(axis=0)
+            image = obs_buffer.max(axis=0)  # TODO: Choose the second frame's confidence and annotation
+            # TODO: For gaze heatmaps, use average or pick one (say the second)
             warped = GrayScaleWarpImage(image)
             max_frames.append(warped)
-    return max_frames
+            anns.append(annotations[i])
+            maps.append(heatmaps[i])
+    return max_frames, anns, maps
 
-def StackFrames(frames):
+def StackFrames(frames, annotations, heatmaps):
     import copy
     """stack every four frames to make an observation (84,84,4)"""
     stacked = []
     stacked_obs = np.zeros((84,84,4))
+    anns = []
+    maps = []
     for i in range(len(frames)):
         if i >= 3:
             stacked_obs[:,:,0] = frames[i-3]
             stacked_obs[:,:,1] = frames[i-2]
             stacked_obs[:,:,2] = frames[i-1]
-            stacked_obs[:,:,3] = frames[i]
+            stacked_obs[:,:,3] = frames[i]    # get audio annotation and heatmap for just this frame
             stacked.append(np.expand_dims(copy.deepcopy(stacked_obs),0))
-    return stacked
 
+            anns.append(annotations[i])
+            maps.append(heatmaps[i])
+    return stacked, anns, maps
 
 
 
@@ -66,60 +75,76 @@ def get_sorted_traj_indices(env_name, dataset):
     g = env_name
     #Note, we're only keeping the full demonstrations that end in terminal to avoid people who quit before the game was over
     traj_indices = []
-    traj_scores = []
+    traj_scores  = []
+    traj_ann     = []
+    traj_heatmap = []
     print(dataset.trajectories.keys())
     for t in dataset.trajectories[g]:
         if env_name == "revenge":
             traj_indices.append(t)
             traj_scores.append(dataset.trajectories[g][t][-1]['score'])
+            traj_ann.append(dataset.trajectories[g][t][-1]['ann'])
+            traj_heatmap.append(dataset.trajectories[g][t][-1]['heatmap'])
 
         elif dataset.trajectories[g][t][-1]['terminal']:
             traj_indices.append(t)
             traj_scores.append(dataset.trajectories[g][t][-1]['score'])
+            traj_ann.append(dataset.trajectories[g][t][-1]['ann'])
+            traj_heatmap.append(dataset.trajectories[g][t][-1]['heatmap'])
+
+    annotations = dataset.annotations[g]
+    heatmaps    = dataset.annotations[g]
 
     sorted_traj_indices = [x for _, x in sorted(zip(traj_scores, traj_indices), key=lambda pair: pair[0])]
     sorted_traj_scores = sorted(traj_scores)
 
     print(sorted_traj_scores)
-    #print(len(sorted_traj_scores))
+    print(sorted_traj_indices)
     print("Max human score", max(sorted_traj_scores))
     print("Min human score", min(sorted_traj_scores))
 
+    sorted_traj_ann     = []
+    sorted_traj_heatmap = []
+    for i in sorted_traj_indices:
+        sorted_traj_ann.append(traj_ann[i-1])
+        sorted_traj_heatmap.append(traj_heatmap[i-1])
     
     seen_scores = set()
     non_duplicates = []
-    for i,s in zip(sorted_traj_indices, sorted_traj_scores):
+    for i,s,a,h in zip(sorted_traj_indices, sorted_traj_scores, sorted_traj_ann, sorted_traj_heatmap):
         if s not in seen_scores:
             seen_scores.add(s)
-            non_duplicates.append((i,s))
+            non_duplicates.append((i,s,a,h))
     print("num non duplicate scores", len(seen_scores))
-    num_demos = 12
-    if env_name == "spaceinvaders":
-        start = 0
-        skip = 4
-    elif env_name == "revenge":
-        start = 0
-        skip = 1
-    elif env_name == "qbert":
-        start = 0
-        skip = 3
-    elif env_name == "mspacman":
-        start = 0
-        skip = 3
-    elif env_name == "pinball":
-        start = 0
-        skip = 1
-    # TODO: Double check skip values
-    elif env_name == "seaquest":
-        start = 0
-        skip = 3
-    elif env_name == "enduro":
-        start = 0
-        skip = 3
+    #num_demos = 12
+    #if env_name == "spaceinvaders":
+    #    start = 0
+    #    skip = 4
+    #elif env_name == "revenge":
+    #    start = 0
+    #    skip = 1
+    #elif env_name == "qbert":
+    #    start = 0
+    #    skip = 3
+    #elif env_name == "mspacman":
+    #    start = 0
+    #    skip = 3
+    #elif env_name == "pinball":
+    #    start = 0
+    #    skip = 1
+    ## TODO: Double check skip values
+    #elif env_name == "seaquest":
+    #    start = 0
+    #    skip = 3
+    #elif env_name == "enduro":
+    #    start = 0
+    #    skip = 3
 
-    demos = non_duplicates[start:num_demos*skip + start:skip]
-    print("(index, score) pairs:",demos)
-    return demos
+    #demos = non_duplicates[start:num_demos*skip + start:skip]
+    #print("(index, score) pairs:", len(demos))
+    #return demos
+    print("(index, score) pairs:", len(non_duplicates))
+    return non_duplicates, annotations, heatmaps
 
 def get_preprocessed_trajectories(env_name, dataset, data_dir, preprocess_name):
     """returns an array of trajectories corresponding to what you would get running checkpoints from PPO
@@ -129,21 +154,25 @@ def get_preprocessed_trajectories(env_name, dataset, data_dir, preprocess_name):
 
    
     print("generating human demos for", env_name)
-    demos = get_sorted_traj_indices(env_name, dataset)
-    human_scores = []
-    human_demos = []
-    for indx, score in demos:
+    demos, annotations, heatmaps = get_sorted_traj_indices(env_name, dataset)
+    human_scores  = []
+    human_demos   = []
+    human_ann     = []
+    human_heatmap = []
+    for indx, score, ann, heatmap in demos:
         human_scores.append(score)
         traj_dir = path.join(data_dir, 'screens', env_name, str(indx))
         #print("generating traj from", traj_dir)
-        maxed_traj = MaxSkipAndWarpFrames(traj_dir)
-        stacked_traj = StackFrames(maxed_traj)
+        maxed_traj, filtered_ann, filtered_heatmap = MaxSkipAndWarpFrames(traj_dir, annotations[indx], heatmaps[indx])
+        stacked_traj, filtered_ann, filtered_heatmap = StackFrames(maxed_traj, filtered_ann, filtered_heatmap)
         demo_norm_mask = []
         #normalize values to be between 0 and 1 and have top part masked
         for ob in stacked_traj:
             demo_norm_mask.append(preprocess(ob, preprocess_name)[0])
         human_demos.append(demo_norm_mask)
-    return human_demos, human_scores
+        human_ann.append(filtered_ann)
+        human_heatmap.append(filtered_heatmap)
+    return human_demos, human_scores, human_ann, human_heatmap
 
 
 if __name__ == "__main__":
@@ -156,7 +185,7 @@ if __name__ == "__main__":
     args.datadir
     data_dir = args.datadir
     dataset = ds.AtariDataset(data_dir)
-    human_demos, human_scores = get_preprocessed_trajectories(env_name, dataset, data_dir, args.atari_full_env)
+    human_demos, human_scores, human_ann, human_heatmap = get_preprocessed_trajectories(env_name, dataset, data_dir, args.atari_full_env)
     print(human_scores)
     
     print("max", max(human_scores))
