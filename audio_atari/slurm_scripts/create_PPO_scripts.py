c9581e08c61a4a94fc49096fd45becf1f01d91e2
Akanksha <akankshasaran@utexas.edu>
2021-10-21 14:49:28 -0500

    generalize to ablation exps

--- audio_atari/slurm_scripts/create_PPO_scripts.py
+++ audio_atari/slurm_scripts/create_PPO_scripts.py
@@ -2,22 +2,26 @@
 # env_names = [['seaquest','Seaquest'],['spaceinvaders','SpaceInvaders'],['mspacman','MsPacman'],['enduro','Enduro'],['montezumarevenge','MontezumaRevenge']]
 # env = ['seaquest','Seaquest']
 # env = ['spaceinvaders','SpaceInvaders']
-# env = ['mspacman','MsPacman']
+env = ['mspacman','MsPacman']
 # env = ['enduro','Enduro']
-env = ['montezumarevenge','MontezumaRevenge']
+# env = ['montezumarevenge','MontezumaRevenge']
 
-# server = 'dgx'
-server = 'titans' 
+# reward_dir = 'learned_reward_models/'
+reward_dir = 'multimodal_reward_models/'+env[0]+'_CAL_only/'
+extra = 'cal_'
+
+server = 'dgx'
+# server = 'titans' 
 seeds = ['0','1','2']
 
 for seed in seeds:
-    bash_file_name = 'PPO/PPO_'+env[0]+'_'+seed+'.sh'
+    bash_file_name = 'PPO/PPO_'+extra+env[0]+'_'+seed+'.sh'
     f = open(bash_file_name,'w')
     f.write("#!/bin/bash\n\n")
 
-    f.write('#SBATCH --job-name PPO_'+env[0]+'_'+seed+'\n')
-    f.write('#SBATCH --output=logs/PPO_'+env[0]+'_'+seed+'_%j.out\n')
-    f.write('#SBATCH --error=logs/PPO_'+env[0]+'_'+seed+'_%j.err\n')
+    f.write('#SBATCH --job-name PPO_'+extra+env[0]+'_'+seed+'\n')
+    f.write('#SBATCH --output=logs/PPO_'+extra+env[0]+'_'+seed+'_%j.out\n')
+    f.write('#SBATCH --error=logs/PPO_'+extra+env[0]+'_'+seed+'_%j.err\n')
     f.write('#SBATCH --mail-user=akankshasaran@utexas.edu\n')
     f.write('#SBATCH --mail-type=END,FAIL,REQUEUE\n')
     f.write('#SBATCH --partition '+server+'\n')
@@ -28,6 +32,6 @@ for seed in seeds:
     f.write('#SBATCH --mem=20G\n')
     f.write('#SBATCH --cpus-per-task=4\n')
 
-    f.write('OPENAI_LOG_FORMAT=\'stdout,log,csv,tensorboard\' OPENAI_LOGDIR=ppo_models/'+env[0]+'_'+seed+' python -m baselines.run --alg=ppo2 --env='+env[1]+'NoFrameskip-v4 --custom_reward pytorch --custom_reward_path learned_reward_models/'+env[0]+'.params --seed '+seed+' --num_timesteps=5e7 --save_interval=500 --num_env 9')
+    f.write('OPENAI_LOG_FORMAT=\'stdout,log,csv,tensorboard\' OPENAI_LOGDIR=ppo_models/'+env[0]+'_'+extra+seed+' python -m baselines.run --alg=ppo2 --env='+env[1]+'NoFrameskip-v4 --custom_reward pytorch --custom_reward_path '+reward_dir+env[0]+'.params --seed '+seed+' --num_timesteps=5e7 --save_interval=500 --num_env 9')
         
     f.close()
