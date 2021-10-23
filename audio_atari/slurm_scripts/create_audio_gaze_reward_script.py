166f5228785cd243d2d625f1b6c877f8511841ee
Akanksha <akankshasaran@utexas.edu>
2021-10-22 19:45:50 -0500

    scripts to create submit files

--- audio_atari/slurm_scripts/create_audio_gaze_reward_script.py
+++ audio_atari/slurm_scripts/create_audio_gaze_reward_script.py
@@ -1,17 +1,21 @@
-
 # env_names = [['seaquest','Seaquest'],['spaceinvaders','SpaceInvaders'],['mspacman','MsPacman'],['enduro','Enduro'],['montezumarevenge','MontezumaRevenge']]
 # env = ['seaquest','Seaquest']
 env = ['spaceinvaders','SpaceInvaders']
 # env = ['mspacman','MsPacman']
 server = 'titans' #'dgx'
 
-bash_file_name = 'reward/reward_CGL_'+env[0]+'.sh'
+extra, extra_option = '_rl_cgl', 'rl_cgl'
+# extra, extra_option = '_rl_cal', 'rl_cal'
+# extra, extra_option = '_cgl_cal', 'cgl_cal'
+# extra, extra_option = '_rl_cgl_cal', 'rl_cgl_cal'
+
+bash_file_name = 'reward/reward'+extra+'_'+env[0]+'.sh'
 f = open(bash_file_name,'w')
 f.write("#!/bin/bash\n\n")
 
-f.write('#SBATCH --job-name reward_cgl_'+env[0]+'\n')
-f.write('#SBATCH --output=logs/reward_cgl_'+env[0]+'_%j.out\n')
-f.write('#SBATCH --error=logs/reward_cgl_'+env[0]+'_%j.err\n')
+f.write('#SBATCH --job-name reward'+extra+'_'+env[0]+'\n')
+f.write('#SBATCH --output=logs/reward'+extra+'_'+env[0]+'_%j.out\n')
+f.write('#SBATCH --error=logs/reward'+extra+'_'+env[0]+'_%j.err\n')
 f.write('#SBATCH --mail-user=akankshasaran@utexas.edu\n')
 f.write('#SBATCH --mail-type=END,FAIL,REQUEUE\n')
 f.write('#SBATCH --partition '+server+'\n')
@@ -22,6 +26,6 @@ f.write('#SBATCH --gres=gpu:1\n')
 f.write('#SBATCH --mem=20G\n')
 f.write('#SBATCH --cpus-per-task=4\n')
 
-f.write('python LearnGazeRewardAGC.py --env_name '+env[0]+' --data_dir ./frames --reward_model_path ./multimodal_reward_models/'+env[0]+'_CGL_only --gaze_loss_only')
+f.write('python LearnAudioGazeRewardAGC.py --env_name '+env[0]+' --data_dir ./frames --reward_model_path ./multimodal_reward_models/'+env[0]+extra+' --loss_type '+extra_option)
       
 f.close()
